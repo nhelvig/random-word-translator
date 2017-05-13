01b71187d1edd570be3bc5d3ef239e6538f98812
nhelvig <nhelvig@thoughtworks.com>
2017-05-13 19:03:27 -0300

    Handles more than one definition. Also fixed formatting

--- word/wordGenerator.go
+++ word/wordGenerator.go
@@ -1,43 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"random-word-translator/generator"
-	"net/http"
-	"net/url"
-	"strings"
-	"encoding/json"
-	"os"
-	"strconv"
+    "fmt"
+    "random-word-translator/generator"
+    "net/http"
+    "net/url"
+    "strings"
+    "encoding/json"
+    "os"
+    "strconv"
 )
 
 var translatorAPI = "https://translate.yandex.net/api/v1.5/tr.json/translate"
 var api_key = os.Getenv("TRANSLATION_API_KEY")
-// var api_key = "trnsl.1.1.20170513T204309Z.5861366a1aa3a27b.f3ebfce9182383782687fab1a4ae61432f3fa86b"
 var language = "pt"
 
 func main() {
-	word := generator.GenerateRandomWord()
-	request := buildRequest(word, language)
-	client := http.Client{}
-	resp, err := client.Do(request)
-	if err != nil {
-		panic(err)
-	}
+    word := generator.GenerateRandomWord()
+    request := buildRequest(word, language)
+    client := http.Client{}
+    resp, err := client.Do(request)
+    if err != nil {
+        panic(err)
+    }
     apiResponse := handleResponse(resp)
 
-    fmt.Println("A tradução de \"" + word + "\" é: " + apiResponse.Text[0] + "\n")
+    for _, translatedWord := range apiResponse.Text {
+          fmt.Println("Um tradução de \"" + word + "\" é: " + translatedWord + "\n")
+    }
+
 }
 
 func buildRequest(word string, language string) *http.Request {
-	form := url.Values{}
+    form := url.Values{}
     form.Add("key", api_key)
     form.Add("text", word)
     form.Add("lang", "en-" + language)
 
     req, err := http.NewRequest("POST", translatorAPI, strings.NewReader(form.Encode()))
     if err != nil {
-    	fmt.Println("Something went wrong building the HTTP request")
+        fmt.Println("Something went wrong building the HTTP request")
         panic(err)
     }
     req.PostForm = form
@@ -48,7 +50,7 @@ func buildRequest(word string, language string) *http.Request {
 }
 
 func handleResponse(resp *http.Response) *Translation {
-	defer resp.Body.Close()
+    defer resp.Body.Close()
 
     translation := new(Translation)
     err := json.NewDecoder(resp.Body).Decode(translation)
@@ -57,9 +59,9 @@ func handleResponse(resp *http.Response) *Translation {
         fmt.Println("Error unmarshalling:", err)
     }
     if translation.Code != 200 {
-    	fmt.Println("Error code received from the translation API: ", 
-    		strconv.Itoa(translation.Code) + " - " + errorResponseCodes[translation.Code])
-    	os.Exit(1)
+        fmt.Println("Error code received from the translation API: ", 
+            strconv.Itoa(translation.Code) + " - " + errorResponseCodes[translation.Code])
+        os.Exit(1)
     }
     return translation
 }
@@ -71,9 +73,9 @@ type Translation struct {
 }
 
 var errorResponseCodes = map[int]string {
-	401 : "Invalid API key",
-	402	: "Blocked API key",
-	404	: "Exceeded the daily limit on the amount of translated text",
-	413	: "Exceeded the maximum text size",
-	422	: "The text cannot be translated",
-	501	: "The specified translation direction is not supported"}
+    401 : "Invalid API key",
+    402    : "Blocked API key",
+    404    : "Exceeded the daily limit on the amount of translated text",
+    413    : "Exceeded the maximum text size",
+    422    : "The text cannot be translated",
+    501    : "The specified translation direction is not supported"}
