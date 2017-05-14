762a5cbabedc62aeeca90a8bf433aafb6feaec35
nhelvig <nhelvig@thoughtworks.com>
2017-05-14 15:38:58 -0300

    Add the image building to the main script

--- startContainer.sh
+++ startContainer.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+docker build -t random_word_translator .
+
 if [[ -n "$1" ]]; then
 	PORT=$1
 else
 	PORT=9001
 fi
+echo "The application should be reachable at http://localhost:$PORT/word/"
 
-docker run -p $PORT:8080 -it word
+docker run -p $PORT:8080 -it random_word_translator
